1b991435ca91ac70679bdf7cc4d7b796f9312e66
Daniel Galvis Torres <1107518001@academico.icesi.edu.co>
2019-05-13 12:40:57 -0500

    Package solReclamo created

--- Paquetes/Nivel1/pkSolReclamo.sql
+++ Paquetes/Nivel1/pkSolReclamo.sql
@@ -1,11 +1,11 @@
-CREATE OR REPLACE PACKAGE pkSolCreacion AS -- spec
+CREATE OR REPLACE PACKAGE pkSolReclamo AS -- spec
 PROCEDURE pInsertar (ivCodigo solicitud.codigo%TYPE,ivEstado solicitud.estado%TYPE,ivDescripcion solicitud.descripcion%TYPE,ivClienteCedula solicitud.cliente_cedula%TYPE, ivProductoCodigo solicitud.producto_codigo%TYPE, ivFechaCracion solicitud.fechaCreacion%TYPE);
 PROCEDURE pBorrar (ivCodigo solicitud.codigo%TYPE);
 PROCEDURE pModificar (ivCodigo solicitud.codigo%TYPE,ivEstado solicitud.estado%TYPE,ivDescripcion solicitud.descripcion%TYPE,ivClienteCedula solicitud.cliente_cedula%TYPE, ivProductoCodigo solicitud.producto_codigo%TYPE, ivFechaCracion solicitud.fechaCreacion%TYPE);
 FUNCTION fConsultar (ivCodigo solicitud.codigo%TYPE) RETURN VARCHAR2;
 END pkSolCreacion;
 /
-CREATE OR REPLACE PACKAGE BODY pkSolCreacion AS
+CREATE OR REPLACE PACKAGE BODY pkSolReclamo AS
 --insertar
 PROCEDURE pInsertar (ivCodigo solicitud.codigo%TYPE,ivEstado solicitud.estado%TYPE,ivDescripcion solicitud.descripcion%TYPE,ivClienteCedula solicitud.cliente_cedula%TYPE, ivProductoCodigo solicitud.producto_codigo%TYPE, ivFechaCracion solicitud.fechaCreacion%TYPE) IS
 BEGIN
@@ -40,16 +40,16 @@ ovconsulta VARCHAR2(5000);
 BEGIN
 SELECT codigo,estado,descripcion,cliente_cedula,producto_codigo,fechacreacion
     INTO vCodigo,vEstado,vDescripcion,vClienteCedula,vProductoCodigo,vFechaCreacion
-    FROM solcreacion 
+    FROM solreclamo 
     INNER JOIN solicitud 
-    ON solicitud.codigo=solcreacion.solicitud_codigo 
+    ON solicitud.codigo=solreclamo.solicitud_codigo 
     WHERE codigo=ivCodigo;
 ovconsulta:= vCodigo||','||vEstado||','||vDescripcion||','||vClienteCedula||','||vProductoCodigo||','||vFechaCreacion;
 RETURN ovconsulta;
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
-    RAISE_APPLICATION_ERROR(-20001,'Error, no existe solicitacion de creación de producto con ese id');
+    RAISE_APPLICATION_ERROR(-20001,'Error, no existe solicitación de reclamo de producto con ese id');
     WHEN OTHERS THEN
     RAISE_APPLICATION_ERROR(-20001,'Error desconocido'||SQLERRM||SQLCODE);
 END fConsultar;
-END pkSolCreacion;
+END pkSolReclamo;
