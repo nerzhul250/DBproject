07c50e2f6d5134b58d7d700fe0762dd36a3e84ce
Daniel Galvis Torres <1107518001@academico.icesi.edu.co>
2019-05-13 12:31:23 -0500

    package pkSolCreacion created

---
+++ Paquetes/Nivel1/pkSolCreacion.sql
@@ -0,0 +1,55 @@
+CREATE OR REPLACE PACKAGE pkSolCreacion AS -- spec
+PROCEDURE pInsertar (ivCodigo solicitud.codigo%TYPE,ivEstado solicitud.estado%TYPE,ivDescripcion solicitud.descripcion%TYPE,ivClienteCedula solicitud.cliente_cedula%TYPE, ivProductoCodigo solicitud.producto_codigo%TYPE, ivFechaCracion solicitud.fechaCreacion%TYPE);
+PROCEDURE pBorrar (ivCodigo solicitud.codigo%TYPE);
+PROCEDURE pModificar (ivCodigo solicitud.codigo%TYPE,ivEstado solicitud.estado%TYPE,ivDescripcion solicitud.descripcion%TYPE,ivClienteCedula solicitud.cliente_cedula%TYPE, ivProductoCodigo solicitud.producto_codigo%TYPE, ivFechaCracion solicitud.fechaCreacion%TYPE);
+FUNCTION fConsultar (ivCodigo solicitud.codigo%TYPE) RETURN VARCHAR2;
+END pkSolCreacion;
+/
+CREATE OR REPLACE PACKAGE BODY pkSolCreacion AS
+--insertar
+PROCEDURE pInsertar (ivCodigo solicitud.codigo%TYPE,ivEstado solicitud.estado%TYPE,ivDescripcion solicitud.descripcion%TYPE,ivClienteCedula solicitud.cliente_cedula%TYPE, ivProductoCodigo solicitud.producto_codigo%TYPE, ivFechaCracion solicitud.fechaCreacion%TYPE) IS
+BEGIN
+INSERT INTO solicitud VALUES (ivCodigo,ivEstado,ivDescripcion,ivClienteCedula,ivProductoCodigo,ivFechaCracion);
+INSERT INTO solcreacion VALUES (ivCodigo);
+END pInsertar;
+
+--borrar
+PROCEDURE pBorrar (ivCodigo solicitud.codigo%TYPE) IS
+BEGIN
+DELETE FROM solcreacion WHERE solicitud_codigo = ivCodigo;
+DELETE FROM solicitud WHERE codigo = ivCodigo;
+END pBorrar;
+
+--modificar
+PROCEDURE pModificar (ivCodigo solicitud.codigo%TYPE,ivEstado solicitud.estado%TYPE,ivDescripcion solicitud.descripcion%TYPE,ivClienteCedula solicitud.cliente_cedula%TYPE, ivProductoCodigo solicitud.producto_codigo%TYPE, ivFechaCracion solicitud.fechaCreacion%TYPE) IS
+BEGIN
+UPDATE solicitud 
+SET estado=ivEstado,descripcion=ivDescripcion,cliente_cedula=ivClienteCedula,producto_codigo=ivProductoCodigo,fechacreacion=ivFechaCracion 
+WHERE codigo = ivCodigo;
+END pModificar;
+
+--consultar
+FUNCTION fConsultar (ivCodigo solicitud.codigo%TYPE) RETURN VARCHAR2 IS
+vCodigo solicitud.codigo%TYPE;
+vEstado solicitud.estado%TYPE;
+vDescripcion solicitud.descripcion%TYPE;
+vClienteCedula solicitud.cliente_cedula%TYPE;
+vProductoCodigo solicitud.producto_codigo%TYPE;
+vFechaCreacion solicitud.fechacreacion%TYPE;
+ovconsulta VARCHAR2(5000);
+BEGIN
+SELECT codigo,estado,descripcion,cliente_cedula,producto_codigo,fechacreacion
+    INTO vCodigo,vEstado,vDescripcion,vClienteCedula,vProductoCodigo,vFechaCreacion
+    FROM solcreacion 
+    INNER JOIN solicitud 
+    ON solicitud.codigo=solcreacion.solicitud_codigo 
+    WHERE codigo=ivCodigo;
+ovconsulta:= vCodigo||','||vEstado||','||vDescripcion||','||vClienteCedula||','||vProductoCodigo||','||vFechaCreacion;
+RETURN ovconsulta;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+    RAISE_APPLICATION_ERROR(-20001,'Error, no existe solicito de creación de producto con ese id');
+    WHEN OTHERS THEN
+    RAISE_APPLICATION_ERROR(-20001,'Error desconocido'||SQLERRM||SQLCODE);
+END fConsultarSolCancelacionProducto;
+END pkSolCancelacionProducto;
